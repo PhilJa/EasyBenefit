0383fcf3d3188405b2bf2d42c4ea98a1fc1ce3bc
PhilJa <philipp.jawny@happyworldtogether.com>
2016-09-30 17:27:37 +0200

    EasyBenefit.de | Version 1.1.8

--- com/common-meta.php
+++ com/common-meta.php
@@ -5,6 +5,13 @@
 <meta name="description" content="Über EasyBenefit.de kannst du mit jedem deiner Onlineeinkäufe kostenlos einen kleinen Betrag an eine Kategorie deiner Wahl spenden. Es sind nur 3 Klicks, die dir die Möglichkeit geben kostenlos etwas Gutes zu tun.">
 <meta name="author" content="EasyBenefit">
 
+<!-- Favicon -->
+<link rel="shortcut icon" href="/img/ico/favicon.ico" type="image/x-icon" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="/img/ico/apple-touch-icon-57-precomposed.png">
+
 <!-- CSS -->
 <link href="/style/css/bootstrap.min.css" rel="stylesheet">
 <link href="/style/css/easybenefit.css" rel="stylesheet">
