ee59efd7cc3548be5220e8849701353182dbe53e
PhilJa <philipp.jawny@happyworldtogether.com>
2016-10-18 13:08:58 +0200

    EasyBenefit.de | Version 1.1.18

---
+++ ueber-uns/index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Über Uns | EasyBenefit.de</title>
+    <!-- Common Meta --><?php include_once("../com/common-meta.php") ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+  <!-- Analytics --><?php include_once("../com/analyticstracking.php") ?>
+  <!-- Spendenstand --><?php include_once("../com/spendenstand.php") ?>
+  <!-- Navigation --><?php include_once("../style/common/navbar.php") ?>
+
+  <!-- Page Content -->
+  <section id="ueberblick" class="paddingx2">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Online einkaufen & Gutes tun.</h1>
+        </div>
+        <div class="col-md-6">
+          <p>
+            Spenden, ohne selbst dafür zu zahlen? <b>Ja, ganz einfach mit nur drei Klicks!</b></br>
+            Indem du vor deinem Einkauf einen unserer Charity-Links benutzt, ewrden bis zu 20% deines Kaufpreises kostelos gespendet. Du zahlst dabei keinen Cent zusätzlich.
+            Mit jedem Einkauf bei einem unserer Zahlreichen Online-Partnershops kannst du so helfen die Welt ein bisschen besser zu machen und hast außerdem etwas Gutes getan.</br></br></p>
+          <h2>Folge uns online:</h2>
+          <p>
+            <a href="https://www.facebook.com/EasyBenefit.de" target="_blank" title="EasyBenefit auf Facebook"><i class="fa fa-facebook-official fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/EasyBenefit" target="_blank" title="EasyBenefit auf Twitter"><i class="fa fa-twitter fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/easybenefit" target="_blank" title="EasyBenefit auf Instagram"><i class="fa fa-instagram fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://plus.google.com/+EasybenefitDe" target="_blank" title="EasyBenefit auf Google+"><i class="fa fa-google-plus-official fa-3x fa-fw" aria-hidden="true"></i></a>
+          </p>
+        </div>
+        <div class="col-md-6">
+          <img src="/style/img/shopping.jpg" class="img-responsive"/>
+        </div>
+      </div><!-- /.row -->
+      <hr>
+      <div class="row">
+				<div class="col-md-4 col-sm-12">
+					<div class="funktion-icon">
+						<i class="fa fa-money fa-fw fa-4x"></i>
+					</div>
+					<div class="funktion-txt">
+						<h2>Kostenlos Spenden</h2>
+						<p>Du zahlst genau den gleichen Betrag wie bei jedem "normalen" Einkauf. Zusätzlich wird in ein Teil des Kaufpreises gespendet.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12">
+					<div class="funktion-icon">
+						<i class="fa fa-sliders fa-fw fa-4x"></i>
+					</div>
+					<div class="funktion-txt">
+						<h2>Selbst entscheiden</h2>
+						<p>Du hast die Wahl. Entscheide dich für eine unserer 9 Spendenkategorien oder verteile deine Spende einfach gleichmäßig auf alle Pools.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-12">
+					<div class="funktion-icon">
+						<i class="fa fa-lightbulb-o fa-fw fa-4x"></i>
+					</div>
+					<div class="funktion-txt">
+						<h2>Weitersagen</h2>
+						<p>Je mehr mitmachen, desto besser. 1% der deutschen Amazon Kunden würden gemeinsam mehrere Millionen jählrich spenden.</p>
+					</div>
+				</div>
+			</div>
+    </div><!-- /.container -->
+  </section><!-- /section -->
+
+  <section id="shopbsp">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Beliebte Partnershops:</h1>
+        </div>
+      </div><!-- /.row -->
+      <div class="row">
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modalamazon">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/001.jpg"/>
+              </div>
+              <div class="shop-description">
+                2-10 %<a class="info" href="/shops/amazon" title="Mehr Infromationen über Charity-Shopping bei Amazon"><i class="fa fa-question-circle-o" aria-hidden="true"></i></a>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modalbahn">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/002.jpg"/>
+              </div>
+              <div class="shop-description">
+                1-5 €
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modaldocmorris">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/003.jpg"/>
+              </div>
+              <div class="shop-description">
+                3-25 %
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modalmydays">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/004.jpg"/>
+              </div>
+              <div class="shop-description">
+                6-11 %
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modalrunnerspoint">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/005.jpg"/>
+              </div>
+              <div class="shop-description">
+                4 %<a class="info" href="/shops/runnerspoint" title="Mehr Infromationen über Charity-Shopping bei RUNNERS POINT"><i class="fa fa-question-circle-o" aria-hidden="true"></i></a>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-md-2 col-sm-3 col-xs-4 smallerpadding">
+          <a data-toggle="modal" data-target="#modalflaconi">
+            <div class="shopcard">
+              <div class="shopimg">
+                <img class="img-responsive" src="/style/img/shops/006.jpg"/>
+              </div>
+              <div class="shop-description">
+                7 %
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div><!-- /.container -->
+  </section><!-- /section -->
+
+
+  <!-- Footer --><?php include_once("../style/common/footer.php") ?>
+  <script src="/style/js/jquery.js"></script>
+  <script src="/style/js/bootstrap.min.js"></script>
+
+    <!-- Modals -->
+    <!--Modal Amazon -->
+    <div class="modal fade" id="modalamazon" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="amazon"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+    <!--Modal Bahn -->
+    <div class="modal fade" id="modalbahn" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="bahn"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+    <!--Modal Docmorris -->
+    <div class="modal fade" id="modaldocmorris" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="docmorris"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+    <!--Modal mydays -->
+    <div class="modal fade" id="modalmydays" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="mydays"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+    <!--Modal runnerspoint -->
+    <div class="modal fade" id="modalrunnerspoint" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="runnerspoint"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+    <!--Modal Flaconi -->
+    <div class="modal fade" id="modalflaconi" tabindex="-1" role="dialog" aria-labelledby="meinModalLabel">
+      <!-- Include Modal --><?php $shop="flaconi"; include "../com/modal.php";?><!-- End Modal -->
+    </div>
+</body>
+</html>
