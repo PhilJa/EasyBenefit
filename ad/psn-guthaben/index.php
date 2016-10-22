bb064c139284303e9c8b22254175e6199a94f672
PhilJa <philipp.jawny@happyworldtogether.com>
2016-10-22 12:29:06 +0200

    EasyBenefit.de | Version 1.1.19

---
+++ ad/psn-guthaben/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>PSN Guthaben aufladen und kostenlos spenden | EasyBenefit.de</title>
+    <!-- Common Meta --><?php include_once("../../com/common-meta.php") ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+  <!-- Analytics --><?php include_once("../../com/analyticstracking.php") ?>
+  <!-- Spendenstand --><?php include_once("../../com/spendenstand.php") ?>
+  <!-- Navigation --><?php include_once("../../style/common/navbar.php") ?>
+
+  <!-- Page Content -->
+  <section id="ueberblick" class="paddingx2 pattern">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>PSN-Guthaben aufladen & kostenlos spenden.</h1>
+        </div>
+        <div class="col-md-6">
+          <p>
+            Während du versuchst das nächste Level zu erreichen oder den Endgegner zu besiegen, gibts es zahlreiche Probleme auf der Welt.
+            Du hast jetzt die Chance mit deiner Guthaben-Aufstockung <b>kostenlos ca. 10%</b> des Wertes zu spenden.</br></br>
+            <b>Alles was du tun musst:</b></br>
+            Fast nichts mehr... Wähle einfach aus um welchen Betrag du dein Guthaben aufstocken möchtes. Wir leiten dich zu Amazon weiter. Dort kaufst du deine Guthabenkarte für den exakt gleichen Preis.
+            Dank unseres Charity-Links spendet Amazon ca. 10 % des Einkaufswertes.</p>
+          <h2>Folge uns online:</h2>
+          <p>
+            <a href="https://www.facebook.com/EasyBenefit.de" target="_blank" title="EasyBenefit auf Facebook"><i class="fa fa-facebook-official fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/EasyBenefit" target="_blank" title="EasyBenefit auf Twitter"><i class="fa fa-twitter fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://www.instagram.com/easybenefit" target="_blank" title="EasyBenefit auf Instagram"><i class="fa fa-instagram fa-3x fa-fw" aria-hidden="true"></i></a>
+            <a href="https://plus.google.com/+EasybenefitDe" target="_blank" title="EasyBenefit auf Google+"><i class="fa fa-google-plus-official fa-3x fa-fw" aria-hidden="true"></i></a>
+          </p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+          <a href="http://amzn.to/2ef71hR" target="_blank">
+					  <img src="/style/img/produkte/25.jpg" class="img-responsive"/>
+          </a>
+        </div>
+        <div class="col-md-3 col-xs-6">
+          <a href="http://amzn.to/2dUIbOM" target="_blank">
+					  <img src="/style/img/produkte/50.jpg" class="img-responsive"/>
+          </a>
+        </div>
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+  </section><!-- /section -->
+
+  <section id="shopbsp">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 style="text-align:center;">Wieviel brauchst du?</h1>
+        </div>
+      </div><!-- /.row -->
+      <div class="row">
+        <div class="col-sm-1 col-sm-offset-1 col-xs-2">
+          <a href="http://amzn.to/2e7pQiZ" target="_blank">
+					  <img src="/style/img/produkte/5.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2eOGKGy" target="_blank">
+					  <img src="/style/img/produkte/10.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2eOGM18" target="_blank">
+					  <img src="/style/img/produkte/15.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2e7o2Xq" target="_blank">
+					  <img src="/style/img/produkte/20.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2ef71hR" target="_blank">
+					  <img src="/style/img/produkte/25.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2dUI4CQ" target="_blank">
+					  <img src="/style/img/produkte/30.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2ef5UyA" target="_blank">
+					  <img src="/style/img/produkte/35.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2e7t2er" target="_blank">
+					  <img src="/style/img/produkte/40.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2dUIbOM" target="_blank">
+					  <img src="/style/img/produkte/50.jpg" class="img-responsive"/>
+          </a>
+				</div>
+        <div class="col-sm-1 col-xs-2">
+          <a href="http://amzn.to/2eOGZkG" target="_blank">
+					  <img src="/style/img/produkte/75.jpg" class="img-responsive"/>
+          </a>
+				</div>
+      </div>
+    </div><!-- /.container -->
+  </section><!-- /section -->
+
+
+  <!-- Footer --><?php include_once("../../style/common/footer.php") ?>
+  <script src="/style/js/jquery.js"></script>
+  <script src="/style/js/bootstrap.min.js"></script>
+</body>
+</html>
--- com/spendenstand.php
+++ com/spendenstand.php
@@ -1,15 +1,15 @@
 <?php
-  $kinder = 196.41; $zielkinder = 1000; $prokinder = $kinder / $zielkinder * 100;
+  $kinder = 208.19; $zielkinder = 1000; $prokinder = $kinder / $zielkinder * 100;
   $senioren = 21.32; $zielsenioren = 1000; $prosenioren = $senioren / $zielsenioren * 100;
   $tiere = 99.63; $zieltiere = 1000; $protiere = $tiere / $zieltiere * 100;
   $natur = 26.33; $zielnatur = 1000; $pronatur = $natur / $zielnatur * 100;
   $entwicklung = 30.14; $zielentwicklung = 1000; $proentwicklung = $entwicklung / $zielentwicklung * 100;
-  $gesundheit = 60.83; $zielgesundheit = 1000; $progesundheit = $gesundheit / $zielgesundheit * 100;
-  $bildung = 55.24; $zielbildung = 1000; $probildung = $bildung / $zielbildung * 100;
-  $armut = 63.95; $zielarmut = 1000; $proarmut = $armut / $zielarmut * 100;
+  $gesundheit = 70.38; $zielgesundheit = 1000; $progesundheit = $gesundheit / $zielgesundheit * 100;
+  $bildung = 55.76; $zielbildung = 1000; $probildung = $bildung / $zielbildung * 100;
+  $armut = 66.20; $zielarmut = 1000; $proarmut = $armut / $zielarmut * 100;
   $sport = 29.23; $zielsport = 1000; $prosport = $sport / $zielsport * 100;
   $gesamt = $kinder + $senioren + $tiere + $natur + $gesundheit + $bildung + $entwicklung + $armut + $sport;
-  $artikel = 401;
+  $artikel = 433;
   $durchschnitt = $gesamt / $artikel;
-  $datum = "18.10.2016";
+  $datum = "22.10.2016";
 ?>
